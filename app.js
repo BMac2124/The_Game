ee2e18d1466fd49d66006fe5ba25d4d90c9e3500
BMac2124 <87466319+BMac2124@users.noreply.github.com>
2021-08-16 23:47:26 -0400

    <commit message>

---
+++ app.js
@@ -0,0 +1,32 @@
+/*to link the images to the board and add functionality I will use 
+addEventListner
+setAttribute
+getAttribut
+Math.floor
+Math.random
+some way to recall the data from the array
+Things needed: 
+Card array holding all my images
+Function for shuffling the array when page loaded and when game restarted. 
+
+function with conditional for removing the cards that match from the cardArray and place them to the now empty[]array
+it will also have to take the cards from the index[i] and place them in the corresponding empty index[i]
+This should be some type of (if,elseif,else conditional statement)
+
+I also need a function that takes count of the score maybe using the counter to iterate throught the array. 
+
+I need to then setup a timer function. 
+
+and add a game reload when game is complete. */
+
+
+
+
+
+document.addEventListener('DOMitems') =>{
+// setting up the array of cards using the images I stored in my folder.  
+const cardArray[
+
+    name
+]
+}
